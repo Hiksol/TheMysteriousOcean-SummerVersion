9281bdcb53bf7a70d01ea87c93b5a7cecb2cb9d3
KOSMOGOR <mortalmortal2015@mail.ru>
2026-07-02 14:41:08 +0300

    update: interactable visual

--- Assets/Scripts/Game/Interactable/InteractableActiveVisual.cs
+++ Assets/Scripts/Game/Interactable/InteractableActiveVisual.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(InteractableActive))]
 public class InteractableActiveVisual : MonoBehaviour
 {
-    public List<ParticleSystem> particleSystems;
+    public List<ParticleSystem> particleSystems = new();
+    public List<AudioSource> audioSources = new();
+    public UnityEvent onWorkingOn = new();
+    public UnityEvent onWorkingOff = new();
 
     InteractableActive interactableActive;
 
@@ -16,7 +20,15 @@ public class InteractableActiveVisual : MonoBehaviour
     void OnDisable() { interactableActive.onInteractableWorkingChanged.RemoveListener(OnInteractableWorkingChanged); }
 
     void OnInteractableWorkingChanged(bool isWorking) {
-        if (isWorking) particleSystems.ForEach(ps => ps.Play());
-        else particleSystems.ForEach(ps => ps.Stop());
+        if (particleSystems.Count > 0) {
+            if (isWorking) particleSystems.ForEach(ps => ps.Play());
+            else particleSystems.ForEach(ps => ps.Stop());
+        }
+        if (audioSources.Count > 0) {
+            if (isWorking) audioSources.ForEach(ps => ps.Play());
+            else audioSources.ForEach(ps => ps.Stop());
+        }
+        if (isWorking) onWorkingOn.Invoke();
+        else onWorkingOff.Invoke();
     }
 }
